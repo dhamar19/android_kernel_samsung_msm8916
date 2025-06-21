f1c4408d9a29c0ad4375500149cc2d8525d8279a
Dhamararf <dhamararifkurniawan@gmail.com>
2025-06-21 21:26:03 +0700

    update build kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 export ARCH=arm
-export CROSS_COMPILE=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
+export CROSS_COMPILE=$(pwd)/tool/bin/arm-eabi-
 
 mkdir output
+make mrproper
 
-make -C $(pwd) O=output VARIANT_DEFCONFIG=msm8916_sec_gtelwifi_usa_defconfig msm8916_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
-make -C $(pwd) O=output
+make -C $(pwd) O=output VARIANT_DEFCONFIG=msm8916_sec_j5lte_eur_defconfig msm8916_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+make -j$(nproc --all) -C $(pwd) O=output 2>&1 | tee -a out/compile.log
 
 cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
