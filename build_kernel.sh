d4eccf12d9e8da13168631c443cd66bc209b6699
Dhamararf <dhamararifkurniawan@gmail.com>
2025-06-21 20:22:52 +0700

    update build kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -10,6 +10,6 @@ mkdir output
 make mrproper
 
 make -C $(pwd) O=output VARIANT_DEFCONFIG=msm8916_sec_j5lte_eur_defconfig msm8916_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
-make -C $(pwd) O=output 2>&1 | tee -a output/compile.log
+make -j$(nproc --all) -C $(pwd) O=output 2>&1 | tee -a output/compile.log
 
 cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
