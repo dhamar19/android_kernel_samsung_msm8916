732cfbd342c8c6319572dc58a59f4259781bde6d
Dhamararf <dhamararifkurniawan@gmail.com>
2025-06-21 20:19:58 +0700

    update build kernel.sh for j5lte

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+export KBUILD_BUILD_USER=DhamarAr
+export KBUILD_BUILD_HOST=Ordinary-Being
+export USE_CCACHE=1
 export ARCH=arm
-export CROSS_COMPILE=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
+export CROSS_COMPILE=$(pwd)/tool/bin/arm-eabi-
 
 mkdir output
+make mrproper
 
-make -C $(pwd) O=output VARIANT_DEFCONFIG=msm8916_sec_gtelwifi_usa_defconfig msm8916_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
-make -C $(pwd) O=output
+make -C $(pwd) O=output VARIANT_DEFCONFIG=msm8916_sec_j5lte_eur_defconfig msm8916_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+make -C $(pwd) O=output 2>&1 | tee -a output/compile.log
 
 cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
